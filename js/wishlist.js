6e4ff6d2edaa117585ae9b9300d844e14aab7b9d
LutfulloAsqarov <lutfulloasqarov9@gmail.com>
2024-04-28 23:01:59 +0500

    js-29

--- js/wishlist.js
+++ js/wishlist.js
@@ -4,19 +4,22 @@ let wishlist = JSON.parse(localStorage.getItem("wishlist"));
 
 function createCards(data) {
     let productCards = "";
+    if (productCards.length === 0) {
+        cards.innerHTML = `<div class="product__no"><h1>NO PRODUCT</h1></div>`;
+    }
     data.forEach((product) => {
         productCards += `
-            <div class="card" data-id=${product.id}>
-                <div class="card__img" >
-                    <img class="product-img" src="${product.images[0]}"  alt="" />
-                    <i class="fa-regular fa-heart"></i>
+                <div class="card" data-id=${product.id}>
+                    <div class="card__img" >
+                        <img class="product-img" src="${product.images[0]}"  alt="" />
+                        <i class="fa-regular fa-heart"></i>
+                    </div>
+                    <div class="card__info">
+                        <h3 class="card__title">${product.title}</h3>
+                        <p class="card__description">${product.description}$</p>
+                    </div>
                 </div>
-                <div class="card__info">
-                    <h3 class="card__title">${product.title}</h3>
-                    <p class="card__description">${product.description}$</p>
-                </div>
-            </div>
-        `;
+            `;
     });
     cards.innerHTML = productCards;
 }
@@ -27,6 +30,7 @@ const addToWishlist = (id) => {
     let wishlist = JSON.parse(localStorage.getItem("wishlist"));
     let updateWishlist = wishlist.filter((el) => el.id !== +id);
     localStorage.setItem("wishlist", JSON.stringify(updateWishlist));
+
     createCards(updateWishlist);
 };
 
